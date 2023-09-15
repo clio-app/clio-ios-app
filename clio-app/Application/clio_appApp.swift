7137237097deff9bf4404de658d978e8ebf6ded1
ThiagoHBA <thiagohhab@gmail.com>
2023-09-15 10:23:13 -0300

    first commit

---
+++ clio-app/Application/clio_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  clio_appApp.swift
+//  clio-app
+//
+//  Created by Thiago Henrique on 15/09/23.
+//
+
+import SwiftUI
+
+@main
+struct clio_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CreateRoomView()
+        }
+    }
+}
---
+++ clio-app/Source/Presentation/CreateRoomView/CreateRoomPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  CreateRoomPresenter.swift
+//  clio-app
+//
+//  Created by Thiago Henrique on 15/09/23.
+//
+
+import Foundation
