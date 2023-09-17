0e630756253c2fb81879d0c5fe47e4227ace3f1d
Luciana Adriao <lucianaasantos@hotmail.com>
2023-09-17 10:08:50 -0300

    Added swiftUI extension for colors

---
+++ clio-app/Source/Presentation/CreateRoomView/Components/CreateRoomComponents.swift
@@ -0,0 +1,20 @@
+//
+//  CreateRoomComponents.swift
+//  clio-app
+//
+//  Created by Luciana Adrião on 17/09/23.
+//
+
+import SwiftUI
+
+struct CreateRoomComponents: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreateRoomComponents_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateRoomComponents()
+    }
+}
