4cc132a1c344e917e30d6126178651cb3b28fa39
BeatrizLdS <beatriz_l.silva@hotmail.com>
2023-09-20 12:38:34 -0300

    :boom: fix: File disappeared is back

--- clio-app/Source/Presentation/CreateRoomView/KeyboardAdaptative.swift
+++ clio-app/Source/Presentation/CreateRoomView/KeyboardAdaptative.swift
@@ -36,7 +36,7 @@ extension UIApplication {
 extension Publishers {
     static var keyboardHeight: AnyPublisher<CGFloat, Never> {
         let willShow = NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification)
-            .map { $0.keyboardHeight * 0.98 }
+            .map { $0.keyboardHeight * 0.9 }
 
         let willHide = NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)
             .map { _ in CGFloat(0) }
