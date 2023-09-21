cd01aae22bf032c99bb5e6594f63f982c23a1a2a
Luciana Adriao <lucianaasantos@hotmail.com>
2023-09-21 09:53:52 -0300

    ✨ feat: MasterInputView, added textfield, image and submit button. Added focus to textfield, missing scrollview

--- clio-app/Source/Presentation/MasterInput/Components/LimitedInputTextField.swift
+++ clio-app/Source/Presentation/MasterInput/Components/LimitedInputTextField.swift
@@ -12,33 +12,39 @@ struct LimitedInputTextField: View {
     var placeholder: String = "Escreva uma descrição sobre a imagem..."
 
     var body: some View {
-        GeometryReader { geo in
-            VStack {
-                TextField("", text: $inputUser)
-                    .multilineTextAlignment(.leading)
-                    .textFieldStyle(.roundedBorder)
-                    .overlay(
-                        Text(placeholder)
-                            .foregroundColor(Color.gray)
-                            .opacity(inputUser.isEmpty ? 1.0 : 0.0) // Show placeholder only when input is empty
-                    )
-
-                HStack(alignment: .center) {
-                    Spacer()
-                    Text("\(inputUser.count)/280")
-                        .padding(.top, 2)
-                        .foregroundColor(.black)
+        VStack {
+            ScrollView(.vertical){
+                TextField(text: $inputUser, axis: .vertical) {
+                    Text(placeholder)
+                        .foregroundColor(.gray)
                 }
+                .foregroundColor(.black)
+                .lineLimit(5)
+                .multilineTextAlignment(.leading)
+                .overlay(
+                    HStack {
+                        Spacer()
+                    })
             }
-            .padding()
-            .background(BorderedBackground(foregroundColor: .clear, backgroundColor: .clear, hasBorder: false))
-            .background(.white).clipShape(RoundedRectangle(cornerRadius: 15))
+            .scrollIndicators(.hidden)
+
+            HStack(alignment: .center) {
+                Spacer()
+                Text("\(inputUser.count)/280")
+                    .padding(.top, 2)
+                    .foregroundColor(.black)
+            }
+            Spacer()
         }
+        .padding(.horizontal, 14)
+        .padding(.vertical, 12)
+        .background(RoundedRectangle(cornerRadius: 15.0, style: .continuous).stroke(.black, style: .init(lineWidth: 2.0)))
+        .background(.white).clipShape(RoundedRectangle(cornerRadius: 15))
     }
 }
 
 
 
 #Preview {
-    LimitedInputTextField(inputUser: .constant("Abra"))
+    LimitedInputTextField(inputUser: .constant("Abralskdmksjdksa"))
 }
