1026498587931f41bd64c3f8b15421b38255fa7a
Luciana Adriao <lucianaasantos@hotmail.com>
2023-09-22 07:38:57 -0300

    💄 style: UI da do card de input do mestre

--- clio-app/Source/Presentation/MasterInput/MasterInputView.swift
+++ clio-app/Source/Presentation/MasterInput/MasterInputView.swift
@@ -10,35 +10,44 @@ struct MasterInputView: View {
     @State var userEntryText: String
     @State var userInputImage: String = "profile-picture-eye"
 
-    /*private*/ var maxWordCount: Int = 280
+    private let maxWordCount: Int = 280
 
     var body: some View {
-        // TODO: fix resize view when keyboard is being used.
         GeometryReader { geo in
-            VStack(spacing: geo.size.height/10){
+            ScrollView(.vertical) {
+                VStack(spacing: geo.size.height/10){
 
-                // MARK: - Top Header
-                    Header()
-                    .frame(height: geo.size.height * 0.12)
+                    // MARK: - Top Header
+                        Header()
+                        .frame(height: geo.size.height * 0.12)
 
-                // MARK: - Card Content
-                MasterInputCard(userInputImage: $userInputImage, userEntryText: $userEntryText)
-                    .padding()
+                    // MARK: - Card Content
+                    MasterInputCard(userInputImage: $userInputImage, userEntryText: $userEntryText)
+                        .padding()
 
-                // MARK: - Button
-                ActionButton(title: "Enviar", foregroundColor: userEntryText.count <= maxWordCount ? .customPink : .gray, hasBorder: false ){
-                    // TODO: Send description to backend
+                    // MARK: - Button
+                    ActionButton(title: "Enviar", foregroundColor: userEntryText.count <= maxWordCount ? .customPink : .gray, hasBorder: false ){
+                        // TODO: Send description to backend
+                    }
+                    // TODO: Solve repetitive code.
+                    .disabled(userEntryText.count > maxWordCount)
+                    .opacity(userEntryText.count <= maxWordCount ? 1.0 : 0.2)
+                    .frame(width: geo.size.width*0.7, height: geo.size.height * 0.07)
+
                 }
-                .disabled(userEntryText.count > maxWordCount)
-                .opacity(userEntryText.count <= maxWordCount ? 1.0 : 0.3)
-                .frame(height: geo.size.height * 0.07)
+                .frame(maxHeight: geo.size.height * 0.9)
+                .frame(height: geo.size.height)
 
-                // TODO: Solve repetitive code.
             }
-            .padding(.top)
+            .scrollIndicators(.hidden)
+            .padding(.horizontal, 24)
+            .keyboardAdaptive()
         }
-        .padding()
         .background(Color.offWhite)
+        .ignoresSafeArea(.keyboard)
+        .onTapGesture {
+            UIApplication.shared.endEditing()
+        }
     }
 }
 
