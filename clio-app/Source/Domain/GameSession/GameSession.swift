652316771ccb9721fd7759f6a349bdc5f17bdb89
ThiagoHBA <thiagohhab@gmail.com>
2023-10-17 10:51:24 -0300

    template for errors

--- clio-app/Source/Domain/GameSession/GameSession.swift
+++ clio-app/Source/Domain/GameSession/GameSession.swift
@@ -10,8 +10,17 @@ import ClioEntities
 
 final class GameSession: ObservableObject {
     @Published var gameFlowParameters = GameFlowParameters()
+    @Published var alertError = AlertError()
     
     func addPlayerInSession(name: String, image: String) {
+        if gameFlowParameters.players.count > 4 {
+            alertError = AlertError(
+                showAlert: true,
+                errorMessage: "Já foi atingido o máximo de jogadores"
+            )
+            return
+        }
+        
         let newUser = User(id: UUID(), name: name, picture: image)
         gameFlowParameters.players.append(newUser)
     }
@@ -25,4 +34,9 @@ struct GameFlowParameters {
     var players: [User] = []
     var didPlay: [User] = []
     var sessionTheme: String = String()
+}
+
+struct AlertError {
+    var showAlert = false
+    var errorMessage = ""
 }
--- clio-app/Source/Presentation/SingleDevice/PlayersView/PlayersView.swift
+++ clio-app/Source/Presentation/SingleDevice/PlayersView/PlayersView.swift
@@ -11,18 +11,23 @@ struct PlayersView: View {
     @EnvironmentObject var session: GameSession
     
     var body: some View {
-        Text("Players")
-        
-        ForEach(session.gameFlowParameters.players, id: \.id) { player in
-            Text("Player: \(player.name)")
+        VStack {
+            Text("Players")
+            
+            ForEach(session.gameFlowParameters.players, id: \.id) { player in
+                Text("Player: \(player.name)")
+            }
+            
+            Button("Adicionar Player") {
+                session.addPlayerInSession(name: "\(Int.random(in: 0..<10))", image: "")
+            }
+            
+            NavigationLink(destination: RaffleThemeView()) {
+                Text("Start")
+            }
         }
-        
-        Button("Adicionar Player") {
-            session.addPlayerInSession(name: "\(Int.random(in: 0..<10))", image: "")
-        }
-        
-        NavigationLink(destination: RaffleThemeView()) {
-            Text("Start")
+        .alert(isPresented: $session.alertError.showAlert) {
+            Alert(title: Text("Error"), message: Text(session.alertError.errorMessage))
         }
     }
 }
