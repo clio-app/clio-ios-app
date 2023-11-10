e15ee1581cf1d5b891fa1256ea22d430c2097d71
Luciana Adriao <lucianaasantos@hotmail.com>
2023-11-10 09:01:22 -0300

    ✨ feat: prompt view

---
+++ clio-app/Source/Presentation/SingleDevice/CustomFirstPromptView/CustomFirstPrompt.swift
@@ -0,0 +1,18 @@
+//
+//  CustomFirstPrompt.swift
+//  clio-app
+//
+//  Created by Luciana Adrião on 10/11/23.
+//
+
+import SwiftUI
+
+struct CustomFirstPrompt: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomFirstPrompt()
+}
